2021528c70f5d8f36e71d55438b9cf08ea6f35e9
hut5830 <76504794+hut5830@users.noreply.github.com>
2023-02-16 15:58:29 +0700

    First Commit
    Test Commit Github On DeskTop

---
+++ app.js
@@ -0,0 +1,22 @@
+//สำหรับ run application หลัก สำหรับ Run
+
+const express = require(`express`);
+const chalk = require(`chalk`);
+const debug = require(`debug`)(`app`);
+const morgan = require(`morgan`);
+const path = require(`path`); 
+// เข้าถึง path ต่างๆ
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.use(express.static(path.join(__dirname,"/public/")));
+
+app.get("/", (req,res) =>{
+    res.send("Hellos");
+})
+
+app.listen(port, ()=>{
+    debug("Listening on Port :" + chalk.red(port));
+})
