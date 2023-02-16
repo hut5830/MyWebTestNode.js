5c4208bfa31cb8f194c875ffa9394f68ac029bd2
hut5830 <76504794+hut5830@users.noreply.github.com>
2023-02-16 20:02:23 +0700

    Nodemon Restart EP.12 BornToDev

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ const express = require(`express`);
 const chalk = require(`chalk`);
 const debug = require(`debug`)(`app`);
 const morgan = require(`morgan`);
-const path = require(`path`); 
+const path = require(`path`);
 // เข้าถึง path ต่างๆ
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 2000 || 3000 || 4000 || 5000;
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-app.use(express.static(path.join(__dirname,"/public/")));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.get("/", (req,res) =>{
-    res.send("Hellos");
-})
+app.get("/", (req, res) => {
+  res.send("Hellos");
+});
 
-app.listen(port, ()=>{
-    debug("Listening on Port :" + chalk.red(port));
-})
+app.listen(port, () => {
+  debug("Listening on Port :" + chalk.red(port));
+});
