f072ea6112ce03a92a7a7135a4d97cd9c1367f65
hut5830 <76504794+hut5830@users.noreply.github.com>
2023-02-17 12:15:09 +0700

    Lec 19

--- app.js
+++ app.js
@@ -5,19 +5,37 @@ const chalk = require(`chalk`);
 const debug = require(`debug`)(`app`);
 const morgan = require(`morgan`);
 const path = require(`path`);
+const { get } = require("http");
+const contact = express.Router(); //Route1
 // เข้าถึง path ต่างๆ
 const app = express();
 const port = process.env.PORT || 2000 || 3000 || 4000 || 5000;
 
-app.set("views","./src/views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use(morgan("combined"));
 
 app.use(express.static(path.join(__dirname, "/public/")));
 
+//Route2
+app.use("/contact", contact);
+//Route3
+contact.route("/").get((req, res) => {
+  res.render("contact");
+});
+contact.route("/1").get((req, res) => {
+  res.send("Hellowww product1");
+});
+contact.route("/2").get((req, res) => {
+  res.send("Hellowww product2");
+});
+
 app.get("/", (req, res) => {
-  res.render (`index`,{username: "Hut", customer: ["Tatcha","Korn","Chom"]});
+  res.render(`index`, {
+    username: "Hut",
+    customer: ["Tatcha", "Korn", "Chom"],
+  });
 });
 
 app.listen(port, () => {
